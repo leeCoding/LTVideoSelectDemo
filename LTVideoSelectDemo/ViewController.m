b8ae7e7916be6d93145e418926a3fa2f30ab9f93
Jonny_Lee <nscode@sina.com>
2018-03-20 13:11:25 +0800

    再次添加log

--- LTVideoSelectDemo/ViewController.m
+++ LTVideoSelectDemo/ViewController.m
@@ -21,6 +21,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"我是other 提交的分支");
+    NSLog(@"再次提交分支");
+
     self.view.backgroundColor = [UIColor whiteColor];
     
     UIButton *selectBtn = [UIButton buttonWithType:UIButtonTypeCustom];
